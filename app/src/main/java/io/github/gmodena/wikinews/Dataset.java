052450f5088ce11d6c44459972f7cfcf8b6e5b0e
Gabriele Modena <gmodena@pm.me>
2024-08-19 22:52:02 +0200

    app: remove benchmark code.

--- app/src/main/java/io/github/gmodena/wikinews/Dataset.java
+++
@@ -1,6 +0,0 @@
-package io.github.gmodena.wikinews;
-
-import java.util.ArrayList;
-
-public record Dataset(ArrayList<float[]> vectors, ArrayList<Integer> ids) {
-}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,2 @@
 rootProject.name = "searchy"
-include("index", "app")
+include("index")
